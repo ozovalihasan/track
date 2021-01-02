ddd8ceae0befb7da44b110f67a110f89baffa7ed
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-02 20:21:00 +0300

    Add App component to index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-// import App from './App';
-
-
+import App from './container/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App /> 
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
