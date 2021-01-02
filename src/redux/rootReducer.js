62ec638061a752ff31859ce74d01ceb5f7b3fc1d
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-02 20:22:26 +0300

    Update redux store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,12 +1,11 @@
-import { combineReducers } from 'redux';
 import userReducer from './user/userReducer';
 import trackedItemReducer from './trackedItem/trackedItemReducer';
 import pieceReducer from './piece/pieceReducer';
 
-const rootReducer = combineReducers({
+const rootReducer = {
   user: userReducer,
   trackedItem: trackedItemReducer,
   piece: pieceReducer,
-});
+};
 
 export default rootReducer;
