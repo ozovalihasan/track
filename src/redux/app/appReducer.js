788f3ef2539bc19f28c2516c4acc1948e5097e8c
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-07 11:33:03 +0300

    Add app and api reducer to redux

---
+++ src/redux/app/appReducer.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    showList: false,
+  },
+  reducers: {
+    showTrackedItems: state => {
+      state.showList = true;
+    },
+
+    hideTrackedItems: state => {
+      state.showList = false;
+    },
+  },
+
+});
+
+const { actions, reducer } = appSlice;
+
+export const {
+  showTrackedItems,
+  hideTrackedItems,
+} = actions;
+
+export default reducer;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,13 +1,17 @@
 import apiReducer from './api/apiReducer';
+import appReducer from './app/appReducer';
 import userReducer from './user/userReducer';
 import trackedItemReducer from './trackedItem/trackedItemReducer';
 import pieceReducer from './piece/pieceReducer';
+import takenTimeReducer from './takenTime/takenTimeReducer';
 
 const rootReducer = {
+  app: appReducer,
   api: apiReducer,
   user: userReducer,
   trackedItem: trackedItemReducer,
   piece: pieceReducer,
+  takenTime: takenTimeReducer,
 };
 
 export default rootReducer;
