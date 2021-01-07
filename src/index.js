f72f1b6adc2d0f286b3f66bfa1e2cb51fcfe35c0
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-07 11:36:06 +0300

    Update imported App with AppContainer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-import App from './component/App';
+import AppContainer from './component/App/AppContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <AppContainer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
