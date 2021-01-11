1ee6f9e8597fb84f1f5a7041f699209dc434fba2
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-11 16:58:17 +0300

    Fix error raising because of mistake action usage

--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,12 +1,12 @@
 import { Redirect } from 'react-router-dom';
 
-import { addUser } from '../../redux';
+import { fetchUserCreate } from '../../redux';
 import UserFormContainer from '../UserForm/UserFormContainer';
 
 const SignUp = () => (localStorage.token ? (
   <Redirect to={{ pathname: '/' }} />
 ) : (
-  <UserFormContainer fetchUser={addUser} buttonName="Sign Up" />
+  <UserFormContainer fetchUser={fetchUserCreate} buttonName="Sign Up" />
 ));
 
 export default SignUp;
