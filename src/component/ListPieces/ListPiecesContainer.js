6ccc6a380d921ef7861dc9691f9f74039caa01a0
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-12 18:11:14 +0300

    Add a feature to direct to "/one-piece-create"

--- src/component/ListPieces/ListPiecesContainer.js
+++ src/component/ListPieces/ListPiecesContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import { fetchListPieces } from '../../redux';
 
 import ListPieces from './ListPieces';
@@ -20,6 +21,12 @@ const ListPiecesContainer = ({ children }) => {
     filteredPieces = pieces.filter(piece => piece.tracked_item_id === trackedItemId);
   } else {
     filteredPieces = pieces;
+  }
+
+  const history = useHistory();
+
+  if (!filteredPieces.length) {
+    history.push('/one-piece-create');
   }
 
   return (
